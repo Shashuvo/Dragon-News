8419d80f5b2f346ef7e87224f1784f273c6292e2
Shashuvo <shahariat2810@gmail.com>
2026-01-01 02:12:37 +0600

    Navbar Added

--- src/components/Latest/Latest.jsx
+++ src/components/Latest/Latest.jsx
@@ -3,7 +3,7 @@ import Marquee from 'react-fast-marquee';
 
 const Latest = () => {
     return (
-        <div className="flex gap-2 md:gap-5 items-center w-full md:w-10/12 mx-auto mt-7 bg-accent p-2 md:py-4 md:pl-4 md:pr-24">
+        <div className="flex gap-2 md:gap-5 items-center bg-accent p-2 md:py-4 md:pl-4 md:pr-24 shadow-md">
             <h1 className="bg-base-200 text-base-300 px-6 py-2 font-medium text-sm md:text-xl">Latest</h1>
 
             <Marquee pauseOnHover={true} speed={50}>
--- src/layouts/HomeLayout/HomeLayout.jsx
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../../components/Header/Header';
 import Latest from '../../components/Latest/Latest';
+import Navbar from '../../components/Navbar/Navbar';
 
 const HomeLayout = () => {
     return (
         <div>
-            <header><Header></Header></header>
-            <section className="latest-news"><Latest></Latest></section>
+            <header>
+                <Header></Header>
+            </header>
+            <section className="latest-news w-full md:w-11/12 mx-auto mt-4 md:mt-7">
+                <Latest></Latest>
+            </section>
+            <nav className="w-full md:w-11/12 mx-auto mt-2 md:mt-5">
+                <Navbar></Navbar>
+            </nav>
             <main>
                 <section className="nav-left"></section>
                 <section className="main">
