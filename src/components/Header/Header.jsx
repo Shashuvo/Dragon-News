d43fe7d75c00e13be5b5139dc4a592e20349a763
Shashuvo <shahariat2810@gmail.com>
2025-12-31 22:31:17 +0600

    Theme Customization, Font & Date-FNS Integration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className='flex flex-col justify-center items-center pt-10 gap-3'>
+            <img className='w-1/3' src={logo} alt="Logo Image" />
+            <p className='text-secondary text-lg'>Journalism Without Fear or Favour</p>
+            <p className='text-xl text-secondary font-medium'><span className='text-primary'>{format(new Date(), 'EEEE')}</span>{format(new Date(), ', MMMM dd, yyyy')}</p>
         </div>
     );
 };
