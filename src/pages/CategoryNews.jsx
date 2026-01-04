d2810abbd953933e4fb71b1711ab144afd389677
Shashuvo <shahariat2810@gmail.com>
2026-01-04 22:59:36 +0600

    News Filtering done

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
+    // loading data
     const data = useLoaderData();
-    console.log(data);
+    const [news, setNews] = useState([]);
+    // loading id
     const { id } = useParams();
+    useEffect(() => {
+        if (id == '0') {
+            setTimeout(() => {
+                setNews(data);
+            }, 0)
+            return;
+        }
+        else if (id == '1') {
+            const filteredNews = data.filter(news => news.others.is_today_pick == true);
+            setTimeout(() => {
+                setNews(filteredNews);
+            }, 0);
+        }
+        else {
+            const filteredNews = data.filter(news => String(news.category_id) === id);
+
+            setTimeout(() => {
+                setNews(filteredNews);
+            }, 0);
+        }
+    }, [data, id]);
     return (
         <div>
-            <h1>CategoryNews {id}</h1>
+            <h1>{news.length} news Found.</h1>
         </div>
     );
 };
