6e1460940ea9bfdc76477062dcbaa8357f39bef0
Shashuvo <shahariat2810@gmail.com>
2026-01-06 23:52:07 +0600

    navigation for home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
-        </div>
+        <Navigate to="/categories/0"></Navigate>
     );
 };
 
