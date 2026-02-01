e84a7f197f46545c4518a91df192bda3709b5b10
Shashuvo <shahariat2810@gmail.com>
2026-02-01 20:10:13 +0600

    Auth State change added

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    console.log(user);
 
     // Create User with email and password
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    // save user info with reload
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
     // context value
     const authData = {
+        user,
+        setUser,
         createUser,
     }
 
--- src/pages/AuthPages/Register.jsx
+++ src/pages/AuthPages/Register.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import { AuthContext } from '../../contexts/AuthContext';
 
 const Register = () => {
-    const { createUser } = use(AuthContext);
+    const { createUser, setUser } = use(AuthContext);
     const handleRegister = e => {
         e.preventDefault();
         const form = e.target;
@@ -16,7 +16,7 @@ const Register = () => {
         createUser(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
+                setUser(user);
             })
             .catch((error) => {
                 const errorMessage = error.message;
