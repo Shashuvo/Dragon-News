11cd1c08a1f6b17887b612e99e60e293ff002bd4
Shashuvo <shahariat2810@gmail.com>
2026-02-01 20:02:25 +0600

    sign Up with email password added

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+
+    // Create User with email and password
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // context value
+    const authData = {
+        createUser,
+    }
+
     return (
-        <AuthContext>
+        <AuthContext value={authData}>
             {children}
         </AuthContext>
     );
--- src/pages/AuthPages/Register.jsx
+++ src/pages/AuthPages/Register.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const Register = () => {
+    const { createUser } = use(AuthContext);
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo);
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                alert(errorMessage);
+            });
+    }
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div className='card bg-base-100 w-11/12 md:w-full shrink-0 p-8 md:p-16 shadow-2xl'>
                 <h1 className='text-2xl md:text-4xl font-semibold text-center text-primary'>Register your account!</h1>
                 <div className="divider my-6 md:my-10"></div>
-                <div className='card-body'>
-                    <form className='fieldset'>
+                <form onSubmit={handleRegister} className='card-body'>
+                    <fieldset className='fieldset'>
                         <label className="label font-bold text-lg md:text-xl text-primary">Your Name</label>
-                        <input type="text" name="name" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your name" />
+                        <input required type="text" name="name" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your name" />
                         <label className="label font-bold text-lg md:text-xl text-primary">Photo URL</label>
-                        <input type="url" name="photo" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your Photo URL" />
+                        <input required type="url" name="photo" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your Photo URL" />
                         <label className="label font-bold text-lg md:text-xl text-primary">Email address</label>
-                        <input type="email" name="email" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your email address" />
+                        <input required type="email" name="email" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your email address" />
                         <label className="label font-bold text-lg md:text-xl text-primary">Password</label>
-                        <input type="password" name="password" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your password" />
+                        <input required type="password" name="password" className="input w-full mb-3 text-secondary bg-base-300" placeholder="Enter your password" />
                         <label className='label text-xs md:text-[16px]'>
-                            <input type="checkbox" name="terms" className="checkbox" />Accept <span className='font-semibold'>terms & conditions</span>
+                            <input required type="checkbox" name="terms" className="checkbox" />Accept <span className='font-semibold'>terms & conditions</span>
                         </label>
-                        <button className="btn btn-primary text-white my-4">Register</button>
+                        <button type='submit' className="btn btn-primary text-white my-4">Register</button>
                         <p className='text-center font-bold text-md md:text-lg text-primary'>Already Have An Account?
                             <Link to="/auth/login" className='text-base-200 hover:underline'> Login</Link></p>
-                    </form>
-                </div>
+                    </fieldset>
+                </form>
 
             </div>
         </div>
